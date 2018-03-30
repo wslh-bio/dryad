85e414ce87143887bc044acb8060f54309604278
Kelsey Florek <kelseyflorek22@gmail.com>
2018-03-30 14:02:43 -0500

    inital commit

---
+++ condor/lyveset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This script impliments use of the lyve-SET SNP tree construction pipline on the CHTC condor grid.
+#
+#Order of arguments:
+#1. path to reads
+#2. path to reference
+#3. number of cpus
+#4. project name
+
+# Shuffle your reads if they are not already. This command
+# creates a folder interleaved and creates interleaved files
+shuffleSplitReads.pl --numcpus $3 -o interleaved $1/*.fastq.gz
+# Create the project directory
+set_manage.pl --create $4
+# Add reads
+for i in interleaved/*.fastq.gz; do
+  set_manage.pl setTest --add-reads $i
+done;
+# Specify your reference genome
+set_manage.pl $4 --change-reference $2
+
+#launch lyve-SET
+launch_set.pl $4 --numcpus $3
