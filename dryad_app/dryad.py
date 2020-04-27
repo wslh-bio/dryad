062ae0de4cb68c1b9acf207fc766edcde1cd237c
Kelsey Florek <kelseyflorek22@gmail.com>
2020-04-27 12:30:10 -0500

    updated to allow user config

--- dryad_app/dryad.py
+++ dryad_app/dryad.py
@@ -14,7 +14,7 @@ import re, sys
 def main():
     #get nextflow executable
     lib_path = os.path.abspath(os.path.dirname(__file__) + '/' + '../lib')
-    dryad_path = os.path.abspath(os.path.dirname(__file__) + '/' + 'dryad.nf')
+    dryad_path = os.path.abspath(os.path.dirname(__file__))
     nextflow_path = os.path.join(lib_path,'nextflow')
 
     #setup argparser to display help if no arguments
@@ -29,28 +29,48 @@ def main():
     parser.add_argument('--output','-o',metavar="<output_path>",type=str,help="Path to ouput directory, default \"dryad_results\".",default="dryad_results")
     parser.add_argument('--core-genome','-cg',default=False, action="store_true", help="Construct a core-genome tree.")
     parser.add_argument('--snp','-s',default=False, action="store_true", help="Construct a SNP tree. Note: Requires a reference genome in fasta format (-r).")
-    parser.add_argument('-ar',default=False, action="store_true", help="Detect AR mechanisms.")
     parser.add_argument('-r',metavar='<path>', type=str,help="Reference genome for SNP pipeline.")
-    parser.add_argument('--profile',metavar='profile_name', type=str,help="Specify a custom nextflow profile.")
+    parser.add_argument('-ar',default=False, action="store_true", help="Detect AR mechanisms.")
     parser.add_argument('--sep',metavar="sep_chars",type=str,help="Dryad identifies sample names from the name of the read file by splitting the name on the specified separating characters, default \"_\".",default="_")
+    parser.add_argument('--profile', type=str,choices=["docker", "singularity"],help="Nextflow profile. Default will try docker first, then singularity if the docker executable cannot be found.")
+    parser.add_argument('--config','-c', type=str,help="Nextflow custom configureation.")
+    parser.add_argument('--get_config',action="store_true",help="Get a Nextflow configuration template for the chosen workflow.")
+    parser.add_argument('--resume', default="", action="store_const",const="-resume",help="resume a previous run")
+
 
     args = parser.parse_args()
+
+    #give config to user if requested
+    if args.get_config:
+        config_path = os.path.join(dryad_path,"configs/dryad_config_template.config")
+        dest_path = os.path.join(os.getcwd(),date.today().strftime("%y-%m-%d")+"_dryad.config")
+        copyfile(config_path,dest_path)
+        sys.exit()
 
     #check for reference sequence
     if args.snp and args.r == None:
         parser_dryad.print_help()
         print("Please specify a reference sequence for the SNP pipeline.")
-
-    #check if we are using docker or singularity or a different profile
-    if args.profile:
-        profile = args.profile
-    elif which('docker'):
-        profile = 'docker'
-    elif which('singularity'):
-        profile = 'singularity'
-    else:
-        print('Singularity or Docker is not installed or not in found in PATH.')
         sys.exit(1)
+
+    #check if we are using docker or singularity
+    if which('docker'):
+        profile = '-profile docker'
+    elif which('singularity'):
+        profile = '-profile singularity'
+    else:
+        profile = ''
+
+    #check for config or profile
+        config = ""
+        if args.config:
+            config = "-C " + os.path.abspath(args.config)
+            profile = ""
+        elif args.profile:
+            profile = args.profile
+        elif not profile:
+            print('Singularity or Docker is not installed or not in found in PATH.')
+            sys.exit(1)
 
     #build nextflow command
     selections = ""
@@ -64,7 +84,7 @@ def main():
     other_args = f"--name_split_on {args.sep} --outdir {args.output}"
     #build command
     command = nextflow_path
-    command = command + f" {dryad_path} -profile {profile} -resume --reads {args.reads_path} {selections} {other_args}"
+    command = command + f" {config} run {dryad_path}/dryad.nf {profile} {args.resume} --reads {args.reads_path} {selections} {other_args}"
 
     #run command using nextflow in a subprocess
     print("Starting the Dryad pipeline:")
