550139d501eefff345b2de1dea9ae71d85ba2209
Kelsey Florek <kelseyflorek22@gmail.com>
2020-05-15 10:46:49 -0500

    2.0.1 patch

--- packaging/setup.py
+++ packaging/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dryad",
-    version="2.0.0",
+    version="2.0.1",
     author="Kelsey Florek",
     author_email="kelsey.florek@slh.wisc.edu",
     description="A pipeline to construct reference free core-genome or SNP phylogenetic trees for examining prokaryote relatedness in outbreaks.",
