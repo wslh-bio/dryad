933667210d013fcbf2e5a48f80006e30a3b2aeae
Eva Gunawan <gunawanevaa@gmail.com>
2024-04-29 15:48:50 -0500

    Adding workflowmain groovy file to fix compilation issue

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -1,5 +1,5 @@
 //
-// This file holds several functions specific to the main.nf workflow in the nf-core/viralrecon pipeline
+// This file holds several functions specific to the main.nf workflow in the wslh-bio/dryad pipeline
 //
 
 class WorkflowMain {
@@ -9,8 +9,9 @@ class WorkflowMain {
     //
     public static String citation(workflow) {
         return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-            "* The pipeline\n" +
-            "  https://doi.org/10.5281/zenodo.3901628\n\n" +
+            // TODO nf-core: Add Zenodo DOI for pipeline after first release
+            //"* The pipeline\n" +
+            //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
             "* The nf-core framework\n" +
             "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
             "* Software dependencies\n" +
@@ -18,10 +19,10 @@ class WorkflowMain {
     }
 
     //
-    // Generate help string
+    // Print help to screen if required
     //
     public static String help(workflow, params, log) {
-        def command = "nextflow run ${workflow.manifest.name} --input samplesheet.csv --outdir <OUTDIR> --genome 'MN908947.3' -profile docker"
+        def command = "nextflow run ${workflow.manifest.name} --input samplesheet.csv --genome GRCh37 -profile docker"
         def help_string = ''
         help_string += NfcoreTemplate.logo(workflow, params.monochrome_logs)
         help_string += NfcoreSchema.paramsHelp(workflow, params, command)
@@ -31,7 +32,7 @@ class WorkflowMain {
     }
 
     //
-    // Generate parameter summary log string
+    // Print parameter summary log to screen
     //
     public static String paramsSummaryLog(workflow, params, log) {
         def summary_log = ''
@@ -52,109 +53,42 @@ class WorkflowMain {
             System.exit(0)
         }
 
-        // Print workflow version and exit on --version
-        if (params.version) {
-            String workflow_version = NfcoreTemplate.version(workflow)
-            log.info "${workflow.manifest.name} ${workflow_version}"
-            System.exit(0)
-        }
-
-        // Print parameter summary log to screen
-        log.info paramsSummaryLog(workflow, params, log)
-
-        // Warn about using custom configs to provide pipeline parameters
-        NfcoreTemplate.warnParamsProvidedInConfig(workflow, log)
-
         // Validate workflow parameters via the JSON schema
         if (params.validate_params) {
             NfcoreSchema.validateParameters(workflow, params, log)
         }
 
+        // Print parameter summary log to screencd ..
+        
+
+        log.info paramsSummaryLog(workflow, params, log)
+
         // Check that a -profile or Nextflow config has been provided to run the pipeline
         NfcoreTemplate.checkConfigProvided(workflow, log)
 
         // Check that conda channels are set-up correctly
-        if (workflow.profile.tokenize(',').intersect(['conda', 'mamba']).size() >= 1) {
+        if (params.enable_conda) {
             Utils.checkCondaChannels(log)
         }
 
         // Check AWS batch settings
         NfcoreTemplate.awsBatch(workflow, params)
 
-        // Check sequencing platform
-        def platformList = ['illumina', 'nanopore']
-        if (!params.platform) {
-            log.error "Platform not specified with e.g. '--platform illumina'. Valid options: ${platformList.join(', ')}."
+        // Check input has been provided
+        if (!params.input) {
+            log.error "Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'"
             System.exit(1)
-        } else if (!platformList.contains(params.platform)) {
-            log.error "Invalid platform option: '${params.platform}'. Valid options: ${platformList.join(', ')}."
-            System.exit(1)
-        }
-
-        // Check Nextclade dataset parameters
-        if (!params.skip_consensus && !params.skip_nextclade) {
-            if (!params.nextclade_dataset && !params.nextclade_dataset_name) {
-                log.error "Nextclade dataset not specified with '--nextclade_dataset' or '--nextclade_dataset_name'. A list of available datasets can be obtained using the Nextclade 'nextclade dataset list' command."
-                System.exit(1)
-            }
         }
     }
     //
     // Get attribute from genome config file e.g. fasta
     //
-    public static String getGenomeAttribute(params, attribute, log, primer_set='', primer_set_version=0) {
-        def val = ''
-        def support_link =  " The default genome config used by the pipeline can be found here:\n" +
-                            "   - https://github.com/nf-core/configs/blob/master/conf/pipeline/viralrecon/genomes.config\n\n" +
-                            " If you would still like to blame us please come and find us on nf-core Slack:\n" +
-                            "   - https://nf-co.re/viralrecon#contributions-and-support\n" +
-                            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    public static Object getGenomeAttribute(params, attribute) {
         if (params.genomes && params.genome && params.genomes.containsKey(params.genome)) {
-            def genome_map = params.genomes[ params.genome ]
-            if (primer_set) {
-                if (genome_map.containsKey('primer_sets')) {
-                    genome_map = genome_map[ 'primer_sets' ]
-                    if (genome_map.containsKey(primer_set)) {
-                        genome_map = genome_map[ primer_set ]
-                        primer_set_version = primer_set_version.toString()
-                        if (genome_map.containsKey(primer_set_version)) {
-                            genome_map = genome_map[ primer_set_version ]
-                        } else {
-                            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                                " --primer_set_version '${primer_set_version}' not found!\n\n" +
-                                " Currently, the available primer set version keys are: ${genome_map.keySet().join(", ")}\n\n" +
-                                " Please check:\n" +
-                                "   - The value provided to --primer_set_version (currently '${primer_set_version}')\n" +
-                                "   - The value provided to --primer_set (currently '${primer_set}')\n" +
-                                "   - The value provided to --genome (currently '${params.genome}')\n" +
-                                "   - Any custom config files provided to the pipeline.\n\n" + support_link
-                            System.exit(1)
-                        }
-                    } else {
-                        log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                            " --primer_set '${primer_set}' not found!\n\n" +
-                            " Currently, the available primer set keys are: ${genome_map.keySet().join(", ")}\n\n" +
-                            " Please check:\n" +
-                            "   - The value provided to --primer_set (currently '${primer_set}')\n" +
-                            "   - The value provided to --genome (currently '${params.genome}')\n" +
-                            "   - Any custom config files provided to the pipeline.\n\n" + support_link
-                        System.exit(1)
-                    }
-                } else {
-                    log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                        " Genome '${params.genome}' does not contain any primer sets!\n\n" +
-                        " Please check:\n" +
-                        "   - The value provided to --genome (currently '${params.genome}')\n" +
-                        "   - Any custom config files provided to the pipeline.\n\n" + support_link
-                    System.exit(1)
-                }
-            }
-            if (genome_map.containsKey(attribute)) {
-                val = genome_map[ attribute ]
-            } else if (params.genomes[ params.genome ].containsKey(attribute)) {
-                val = params.genomes[ params.genome ][ attribute ]
+            if (params.genomes[ params.genome ].containsKey(attribute)) {
+                return params.genomes[ params.genome ][ attribute ]
             }
         }
-        return val
+        return null
     }
 }
