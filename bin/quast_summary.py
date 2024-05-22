acca4347051a5dcee3e7103d9e3fdc876e710249
Eva Gunawan <gunawanevaa@gmail.com>
2024-05-22 08:21:00 -0500

    Added quast_summary functionality

--- bin/quast_summary.py
+++ bin/quast_summary.py
@@ -2,14 +2,13 @@
 import os
 import glob
 import pandas as pd
-from pandas import DataFrame
 
 # function for summarizing quast output
 def summarize_quast(file):
     # get sample id from file name and set up data list
     sample_id = os.path.basename(file).split(".")[0]
     # read in data frame from file
-    df = pd.read_csv(file, sep='\\t')
+    df = pd.read_csv(file, sep='\t')
     # get contigs, total length and assembly length columns
     df = df.iloc[:,[1,7,17]]
     # assign sample id as column
@@ -21,7 +20,7 @@ def summarize_quast(file):
     return df
 
 # get quast output files
-files = glob.glob("data*/*.transposed.quast.report.tsv*")
+files = glob.glob("data/*.transposed.quast.report.tsv*")
 
 # summarize quast output files
 dfs = map(summarize_quast,files)
@@ -30,7 +29,7 @@ dfs = list(dfs)
 # concatenate dfs and write data frame to file
 if len(dfs) > 1:
     dfs_concat = pd.concat(dfs)
-    dfs_concat.to_csv(f'quast_results.tsv',sep='\\t', index=False, header=True, na_rep='NaN')
+    dfs_concat.to_csv(f'quast_results.tsv',sep='\t', index=False, header=True, na_rep='NaN')
 else:
     dfs = dfs[0]
-    dfs.to_csv(f'quast_results.tsv',sep='\\t', index=False, header=True, na_rep='NaN')
+    dfs.to_csv(f'quast_results.tsv',sep='\t', index=False, header=True, na_rep='NaN')
