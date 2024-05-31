d06ce4350550400a3f616eab53279d2b9ee6f714
Kelsey Florek <kelseyflorek22@gmail.com>
2024-05-31 10:31:57 -0500

    updates to remove genomes params

--- lib/WorkflowDryad.groovy
+++ lib/WorkflowDryad.groovy
@@ -6,20 +6,6 @@ import nextflow.Nextflow
 import groovy.text.SimpleTemplateEngine
 
 class WorkflowDryad {
-
-    //
-    // Check and validate parameters
-    //
-    public static void initialise(params, log) {
-
-        genomeExistsError(params, log)
-
-
-        if (!params.fasta && params.alignment_based == 'true') {
-            Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        }
-    }
-
     //
     // Get workflow summary for MultiQC
     //
@@ -104,19 +90,5 @@ class WorkflowDryad {
         def description_html = engine.createTemplate(methods_text).make(meta)
 
         return description_html
-    }
-
-    //
-    // Exit pipeline if incorrect --genome key provided
-    //
-    private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            def error_string = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            Nextflow.error(error_string)
-        }
     }
 }
