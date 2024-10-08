c34962802bd918b6003707b12b1a28f1f63d7db4
Eva Gunawan <gunawanevaa@gmail.com>
2024-10-08 16:00:57 -0500

    Need to fix. Currently shows all samples

--- bin/compare_io.py
+++ bin/compare_io.py
@@ -2,51 +2,52 @@
 
 import pandas as pd
 import argparse
+import re
 import sys
 
 def parse_args(args=None):
-	Description='Compares the amount of samples input to the pipeline to the samples that exit parsnp'
+	Description='Compares the samples input in the pipeline to the samples that exit parsnp'
 
 	parser = argparse.ArgumentParser(description=Description)
 	parser.add_argument('input_file',
 		help='Samplesheet.valid.csv with all the samples denoted.')
 	parser.add_argument('output_file',
-		help='Parsnp output file to compare input samples to.')
+		help='Parsnp tree output file to compare input samples to.')
 
 	return parser.parse_args(args)
 
 def create_list_of_input(samplesheet):
-	input_list = []
-	count = 0
 
-	with open(samplesheet, "r") as infile:
-		for line in infile:
-			input_list.append(line.split(",")[0])
-			count +=1
+	samplesheet_df = pd.read_csv(samplesheet)
+	all_samples = samplesheet_df['sample'].tolist()
 
-	return input_list, count
+	return all_samples
 
 def check_parsnp_file(parsnp_file, input_list):
-	parsnp_list = []
-	missing_list = []
+
 	with open(parsnp_file, "r") as infile:
-		for line in infile:
-			print(line + '\n')
-			x = line.split(":")
-			print(x)
-			
-			if x in input_list:
-				parsnp_list.append(x)
-			else:
-				if type(x) == 'int':
-					pass
-				else:
-					missing_list.append(x)
+		tree_content = infile.read()
+
+	samples_from_tree = re.findall(r'[^:()]+', tree_content)
+
+	# Clean up sample names, if necessary
+	samples_from_tree = [sample.strip() for sample in samples_from_tree]
+
+	# Step 3: Compare the lists
+	missing_samples = set(input_list) - set(samples_from_tree)
+
+	# Output results
+	if missing_samples:
+		print("Missing samples in parsnp.tree:")
+		for sample in missing_samples:
+			print(sample)
+	else:
+	    print("All samples are present in parsnp.tree.")
 
 def main(args=None):
 	args = parse_args(args)
 
-	total_list, total_count = create_list_of_input(args.input_file)
+	total_list = create_list_of_input(args.input_file)
 	check_parsnp_file(args.output_file, total_list)
 
 if __name__ == "__main__":
